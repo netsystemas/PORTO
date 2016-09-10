5151a68b957ae6d301b05e5b6edf6e7da277a451
netsystemas <netsystemas@gmail.com>
2016-09-10 16:28:35 -0300

    Add files via upload

--- inc/admin/functions.php
+++ inc/admin/functions.php
@@ -104,38 +104,51 @@ function porto_options_footer_types() {
     );
 }
 
-// Modificado por Pets na Web
+function porto_demo_filters() {
+    return array(
+        '*' => 'Show All',
+        'demos' => 'Demos',
+        'classic' => 'Classic',
+        'corporate' => 'Corporate',
+        'shop' => 'Shop',
+        'dark' => 'Dark',
+        'rtl' => 'RTL',
+    );
+}
+
 function porto_demo_types() {
     return array(
-        //'landing' => array('blog_id' => '34','alt' => 'Landing', 'img' => porto_options_uri.'/demos/landing.jpg'),
-        //'classic-original' => array('blog_id' => '9','alt' => 'Classic Original', 'img' => porto_options_uri.'/demos/classic_original.jpg'),
-        //'classic-color' => array('blog_id' => '10','alt' => 'Classic Color', 'img' => porto_options_uri.'/demos/classic_color.jpg'),
-        'classic-light' => array('blog_id' => '14','alt' => 'Classic Light', 'img' => porto_options_uri.'/demos/classic_light.jpg'),
-        //'classic-video' => array('alt' => 'Classic Video', 'img' => porto_options_uri.'/demos/classic_video.jpg'),
-        //'classic-video-light' => array('alt' => 'Classic Video Light', 'img' => porto_options_uri.'/demos/classic_video_light.jpg'),
-        //'corporate1' => array('blog_id' => '13','alt' => 'Corporate 1', 'img' => porto_options_uri.'/demos/corporate_1.jpg'),
-        //'corporate2' => array('blog_id' => '15','alt' => 'Corporate 2', 'img' => porto_options_uri.'/demos/corporate_2.jpg'),
-        'corporate3' => array('blog_id' => '11','alt' => 'Corporate 3', 'img' => porto_options_uri.'/demos/corporate_3.jpg'),
-        //'corporate4' => array('blog_id' => '16','alt' => 'Corporate 4', 'img' => porto_options_uri.'/demos/corporate_4.jpg'),
-        //'corporate5' => array('blog_id' => '17','alt' => 'Corporate 5', 'img' => porto_options_uri.'/demos/corporate_5.jpg'),
-        //'corporate6' => array('blog_id' => '18','alt' => 'Corporate 6', 'img' => porto_options_uri.'/demos/corporate_6.jpg'),
-        'corporate7' => array('blog_id' => '19','alt' => 'Corporate 7', 'img' => porto_options_uri.'/demos/corporate_7.jpg'),
-        'corporate8' => array('blog_id' => '20','alt' => 'Corporate 8', 'img' => porto_options_uri.'/demos/corporate_8.jpg'),
-        'corporate-hosting' => array('blog_id' => '21','alt' => 'Corporate Hosting', 'img' => porto_options_uri.'/demos/corporate_hosting.jpg'),
-        'corporate-digital-agency' => array('blog_id' => '22','alt' => 'Corporate Digital Agency', 'img' => porto_options_uri.'/demos/corporate_digital_agency.jpg'),
-        //'corporate-law-office' => array('blog_id' => '23','alt' => 'Corporate Law Office', 'img' => porto_options_uri.'/demos/corporate_law_office.jpg'),
-        'shop1' => array('blog_id' => '24','alt' => 'Shop 1', 'img' => porto_options_uri.'/demos/shop_1.jpg'),
-        //'shop2' => array('blog_id' => '25','alt' => 'Shop 2', 'img' => porto_options_uri.'/demos/shop_2.jpg'),
-        //'shop3' => array('blog_id' => '26','alt' => 'Shop 3', 'img' => porto_options_uri.'/demos/shop_3.jpg'),
-        //'shop4' => array('blog_id' => '27','alt' => 'Shop 4', 'img' => porto_options_uri.'/demos/shop_4.jpg'),
-        //'shop5' => array('blog_id' => '28','alt' => 'Shop 5', 'img' => porto_options_uri.'/demos/shop_5.jpg'),
-        //'shop6' => array('blog_id' => '29','alt' => 'Shop 6', 'img' => porto_options_uri.'/demos/shop_6.jpg'),
-        //'shop7' => array('blog_id' => '30','alt' => 'Shop 7', 'img' => porto_options_uri.'/demos/shop_7.jpg'),
-        //'shop8' => array('blog_id' => '31','alt' => 'Shop 8', 'img' => porto_options_uri.'/demos/shop_8.jpg'),
-        'shop9' => array('blog_id' => '32','alt' => 'Shop 9', 'img' => porto_options_uri.'/demos/shop_9.jpg'),
-        //'shop10' => array('blog_id' => '33','alt' => 'Shop 10', 'img' => porto_options_uri.'/demos/shop_10.jpg'),
-        //'dark' => array('blog_id' => '12','alt' => 'Dark Original', 'img' => porto_options_uri.'/demos/dark_original.jpg'),
-        //'rtl' => array('blog_id' => '35','alt' => 'RTL Original', 'img' => porto_options_uri.'/demos/rtl_original.jpg'),
+        'classic-original' => array('alt' => 'Main Demo', 'img' => porto_options_uri.'/demos/classic_original.jpg', 'filter' => 'demos'),
+        'construction' => array('alt' => 'Construction', 'img' => porto_options_uri.'/demos/demo_construction.jpg', 'filter' => 'demos'),
+        'restaurant' => array('alt' => 'Restaurant', 'img' => porto_options_uri.'/demos/demo_restaurant.jpg', 'filter' => 'demos'),
+        'digital-agency' => array('alt' => 'Digital Agency', 'img' => porto_options_uri.'/demos/demo_digital_agency.jpg', 'filter' => 'demos'),
+        'law-firm' => array('alt' => 'Law Firm', 'img' => porto_options_uri.'/demos/demo_law_firm.jpg', 'filter' => 'demos'),
+        'landing' => array('alt' => 'Landing', 'img' => porto_options_uri.'/demos/landing.jpg', 'filter' => 'classic'),
+        'classic-color' => array('alt' => 'Classic Color', 'img' => porto_options_uri.'/demos/classic_color.jpg', 'filter' => 'classic'),
+        'classic-light' => array('alt' => 'Classic Light', 'img' => porto_options_uri.'/demos/classic_light.jpg', 'filter' => 'classic'),
+        'classic-video' => array('alt' => 'Classic Video', 'img' => porto_options_uri.'/demos/classic_video.jpg', 'filter' => 'classic'),
+        'classic-video-light' => array('alt' => 'Classic Video Light', 'img' => porto_options_uri.'/demos/classic_video_light.jpg', 'filter' => 'classic'),
+        'corporate1' => array('alt' => 'Corporate 1', 'img' => porto_options_uri.'/demos/corporate_1.jpg', 'filter' => 'corporate'),
+        'corporate2' => array('alt' => 'Corporate 2', 'img' => porto_options_uri.'/demos/corporate_2.jpg', 'filter' => 'corporate'),
+        'corporate3' => array('alt' => 'Corporate 3', 'img' => porto_options_uri.'/demos/corporate_3.jpg', 'filter' => 'corporate'),
+        'corporate4' => array('alt' => 'Corporate 4', 'img' => porto_options_uri.'/demos/corporate_4.jpg', 'filter' => 'corporate'),
+        'corporate5' => array('alt' => 'Corporate 5', 'img' => porto_options_uri.'/demos/corporate_5.jpg', 'filter' => 'corporate'),
+        'corporate6' => array('alt' => 'Corporate 6', 'img' => porto_options_uri.'/demos/corporate_6.jpg', 'filter' => 'corporate'),
+        'corporate7' => array('alt' => 'Corporate 7', 'img' => porto_options_uri.'/demos/corporate_7.jpg', 'filter' => 'corporate'),
+        'corporate8' => array('alt' => 'Corporate 8', 'img' => porto_options_uri.'/demos/corporate_8.jpg', 'filter' => 'corporate'),
+        'corporate-hosting' => array('alt' => 'Corporate Hosting', 'img' => porto_options_uri.'/demos/corporate_hosting.jpg', 'filter' => 'corporate'),
+        'shop1' => array('alt' => 'Shop 1', 'img' => porto_options_uri.'/demos/shop_1.jpg', 'filter' => 'shop'),
+        'shop2' => array('alt' => 'Shop 2', 'img' => porto_options_uri.'/demos/shop_2.jpg', 'filter' => 'shop'),
+        'shop3' => array('alt' => 'Shop 3', 'img' => porto_options_uri.'/demos/shop_3.jpg', 'filter' => 'shop'),
+        'shop4' => array('alt' => 'Shop 4', 'img' => porto_options_uri.'/demos/shop_4.jpg', 'filter' => 'shop'),
+        'shop5' => array('alt' => 'Shop 5', 'img' => porto_options_uri.'/demos/shop_5.jpg', 'filter' => 'shop'),
+        'shop6' => array('alt' => 'Shop 6', 'img' => porto_options_uri.'/demos/shop_6.jpg', 'filter' => 'shop'),
+        'shop7' => array('alt' => 'Shop 7', 'img' => porto_options_uri.'/demos/shop_7.jpg', 'filter' => 'shop'),
+        'shop8' => array('alt' => 'Shop 8', 'img' => porto_options_uri.'/demos/shop_8.jpg', 'filter' => 'shop'),
+        'shop9' => array('alt' => 'Shop 9', 'img' => porto_options_uri.'/demos/shop_9.jpg', 'filter' => 'shop'),
+        'shop10' => array('alt' => 'Shop 10', 'img' => porto_options_uri.'/demos/shop_10.jpg', 'filter' => 'shop'),
+        'dark' => array('alt' => 'Dark Original', 'img' => porto_options_uri.'/demos/dark_original.jpg', 'filter' => 'dark'),
+        'rtl' => array('alt' => 'RTL Original', 'img' => porto_options_uri.'/demos/rtl_original.jpg', 'filter' => 'rtl'),
     );
 }
 
@@ -150,3 +163,22 @@ function porto_options_breadcrumbs_types() {
 
     );
 }
+
+function porto_options_footer_columns() {
+    return array(
+        '1' => __('1 column - 1/12', 'porto'),
+        '2' => __('2 columns - 1/6', 'porto'),
+        '3' => __('3 columns - 1/4', 'porto'),
+        '4' => __('4 columns - 1/3', 'porto'),
+        '5' => __('5 columns - 5/12', 'porto'),
+        '6' => __('6 columns - 1/2', 'porto'),
+        '7' => __('7 columns - 7/12', 'porto'),
+        '8' => __('8 columns - 2/3', 'porto'),
+        '9' => __('9 columns - 3/4', 'porto'),
+        '10' => __('10 columns - 5/6', 'porto'),
+        '11' => __('11 columns - 11/12)', 'porto'),
+        '12' => __('12 columns - 1/1', 'porto')
+    );
+}
+
+
